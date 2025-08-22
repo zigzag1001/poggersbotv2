bd11666b1df0d089edebb35f0fee193d2aec27ec
zigzag1001 <peter.saburov@gmail.com>
2025-08-22 00:09:27 +0000

    .sh file fix

--- restart_for_cron.sh
+++ restart_for_cron.sh
@@ -1,2 +1,6 @@
-0 3 * * * cd /home/opc/bot/poggersbotv2 && sudo docker compose build --no-cache && sudo docker compose up -d && sudo docker image prune -af
+# 0 3 * * *
+cd /home/opc/bot/poggersbotv2 &&
+    sudo docker compose build --no-cache &&
+    sudo docker compose up -d &&
+    sudo docker image prune -af
 # REPLACE DIR WITH YOUR OWN IF USING THIS
